8b2a63ab1a183e825c888c5100a40ce874af5dce
Asha Fernandez <ash.fernan90@gmail.com>
2019-12-20 04:46:07 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,25 +29,23 @@ end
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + '!' }
 end
-
-summon_captain_planet(planeteer_calls)
-
+  summon_captain_planet(planeteer_calls)
+end
 
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
 end
-
-long_planeteer_calls(planeteer_calls)
+  long_planeteer_calls(planeteer_calls)
 end
 
 potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
+end
 
 def find_the_cheese(potentially_cheesy_items)
   cheeses = %w[gouda cheddar camembert]
-
+end
   potentially_cheesy_items.find do |maybe_cheese|
     cheeses.include?(maybe_cheese)
   end
-end
 
 find_the_cheese(potentially_cheesy_items)
