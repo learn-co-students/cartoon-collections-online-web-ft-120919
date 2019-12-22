29cfdc61457f7e2a5af8d95de43a08433790a327
Mikael Kaya <kayaswork@gmail.com>
2019-12-22 04:13:31 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves ["Doc", "Dopey", "Bashful", "Grumpy"]
+ hash = Hash.new
+%w(roll_call_dwarves).each_with_index { |item, index|
+  hash[item] = index
+}
+hash   #=> {"Doc"=>0, "Dopey"=>1, "Bashful"=>2, "Grumpy"=>3}
 end
 
 def summon_captain_planet# code an argument here
@@ -10,7 +14,9 @@ def long_planeteer_calls# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |item|
+    cheese_types.include?(item)
+  end
 end
